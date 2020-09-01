d9f1d449cf4f1a04506047c6fa6892b14753bbb0
Theo Sun <theo.sun@sap.com>
2020-09-01 14:10:10 +0800

    chore(ci): test refacotr

--- test/middleware/onerror.js
+++ test/middleware/onerror.js
@@ -20,13 +20,3 @@ describe('error', () => {
         expect(response.text).toMatch(/Error: Error test/);
     });
 });
-
-describe('httperror', () => {
-    it(`httperror`, async () => {
-        const response = await request.get('/test/httperror');
-        expect(response.status).toBe(404);
-        expect(response.text).toMatch(
-            /Response code 404 \(Not Found\): target website might be blocking our access, you can <a href="https:\/\/docs\.rsshub\.app\/install\/">host your own RSSHub instance<\/a> for a better usability\./
-        );
-    });
-});
