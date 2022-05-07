6af34bab5a47502172d8612846541ab37d3ac041
Theo Sun <theo.sun.github@outlook.com>
2022-05-07 11:58:41 +0800

    feat: jiemian news

---
+++ lib/customize/index.d.ts
@@ -0,0 +1,17 @@
+import type { ParameterizedContext } from "koa"
+import type { Cache } from '../middleware/cache/base'
+export { }
+
+
+declare global {
+    export type RSSHubArticle = {
+        title: string;
+        pubDate: Date | string;
+        link: string;
+        guid: string;
+        description: string;
+        author: string;
+    }
+
+    export type RSSHubContext = ParameterizedContext<{ data: { title: string, link: string, item: Array<RSSHubArticle> } }, { cache: Cache }>
+}
