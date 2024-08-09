8304c616012fb230fe453d1064f4986d4ec022f7
Theo Sun <theo.sun.github@outlook.com>
2024-08-09 22:15:43 +0800

    chore: update

--- test/utils/common-utils.js
+++ test/utils/common-utils.js
@@ -2,8 +2,16 @@ const utils = require("../../lib/utils/common-utils");
 
 describe("common-utils", () => {
   it("toTitleCase", async () => {
-    expect(utils.toTitleCase("RSSHub IS AS aweSOme aS henry")).toBe(
-      "Rsshub Is As Awesome As Henry",
-    );
+    expect(utils.toTitleCase("RSSHub IS AS aweSOme aS henry")).toBe("Rsshub Is As Awesome As Henry");
   });
+
+  it("htmlToMarkdown", () => {
+    expect(utils.htmlToMarkdown("<h1>hello</h1>")).toMatchSnapshot();
+  });
+
+  it("markdownToHtml", () => {
+    expect(utils.markdownToHtml("# hello")).toMatchSnapshot();
+  });
+
+
 });
