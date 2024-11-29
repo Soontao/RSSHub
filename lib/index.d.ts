f82a6e8da3352e50cc180cc1ec4175177cfb4f0d
Theo <theo.sun@outlook.com>
2024-11-30 00:04:32 +0800

    chore: update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -12,8 +12,42 @@ declare global {
     author: string;
   };
 
-  export type RSSHubContext = ParameterizedContext<
-    { data: { title: string; link: string; item: Array<RSSHubArticle> } },
-    { cache: Cache }
+  export type RSSHubContext = ParameterizedContext<{ data: { title: string; link: string; item: Array<RSSHubArticle> } }, { cache: Cache }>;
+  /**
+   * Item 类型定义
+   */
+  export interface Item {
+    title: string;
+    pubDate: string;
+    author: string;
+    link: string;
+    guid: string;
+    description: string;
+  }
+
+  /**
+   * ContextData 类型定义
+   */
+  export interface ContextData {
+    title: string;
+    link: string;
+    item: Array<Item>;
+  }
+
+  /**
+   * ContextCache 类型定义
+   */
+  export type ContextCache = import("@/middleware/cache/base").Cache;
+
+  /**
+   * RSSHubKoaContext 类型定义
+   */
+  export type RSSHubKoaContext = import("koa").ParameterizedContext<
+    {
+      data: ContextData;
+    },
+    {
+      cache: ContextCache;
+    }
   >;
 }
