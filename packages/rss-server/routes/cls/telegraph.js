9d2dbbb843bb3e95cd893a51ec31c4a8f1497db7
Theo Sun <theo.sun@outlook.com>
2025-03-05 12:53:02 +0800

    chore: update telegraph route to use optional chaining and improve date formatting

--- packages/rss-server/routes/cls/telegraph.js
+++ packages/rss-server/routes/cls/telegraph.js
@@ -2,17 +2,14 @@ const got = require("rss-libs/utils/got");
 
 module.exports = async (ctx) => {
   const link = "https://www.cls.cn/nodeapi/updateTelegraphList?rn=20";
-  const response = await got({
-    method: "get",
-    url: link,
-  });
+  const response = await got({ method: "get", url: link });
 
   const list = response.data.data.roll_data.map((item) => ({
-    title: item.title || item.content,
+    title: item?.title ?? item?.content,
     description: item.content,
-    pubDate: new Date(item.ctime * 1000).toUTCString(),
+    pubDate: new Date(item.ctime * 1000).toISOString(),
     guid: item.id,
-    link: `https://api3.cls.cn/share/article/${item.id}`,
+    link: `https://www.cls.cn/detail/${item.id}`,
   }));
 
   ctx.state.data = {
