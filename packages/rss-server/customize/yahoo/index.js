e204c2b9d16d6d5f110f33d28b34057e4fc31ce0
Theo <theo.sun@outlook.com>
2025-04-07 20:08:27 +0800

    chore: remove unused translateTitle and fetchText options from Yahoo News endpoint

--- packages/rss-server/customize/yahoo/index.js
+++ packages/rss-server/customize/yahoo/index.js
@@ -4,10 +4,7 @@ const { fetchTextWithCrossWallProxy } = require("rss-libs/utils/http");
 module.exports = createGenericEndpoint({
   entryUrl: "https://www.yahoo.com/news/",
   endpointPath: "/yahoo-news",
-  translateTitle: true,
   maxItemsInList: 10,
-  fetchText: fetchTextWithCrossWallProxy,
   linkExtractor: linkExtractors.aLinkExtractor("a.js-content-viewer", "https://www.yahoo.com"),
   contentExtractor: contentExtractors.js,
-  language: "en",
 });
