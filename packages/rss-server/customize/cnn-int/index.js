ff5aa4751854978005c1c0cdbb6e37df5ae32016
Theo <theo.sun@outlook.com>
2025-04-07 09:25:42 +0800

    chore: remove unused fetchTextWithCrossWallProxy and translateTitle from cnn-international endpoint

--- packages/rss-server/customize/cnn-int/index.js
+++ packages/rss-server/customize/cnn-int/index.js
@@ -1,14 +1,11 @@
 const { createGenericEndpoint, linkExtractors, contentExtractors } = require("rss-libs/utils/common-utils");
-const { fetchTextWithCrossWallProxy } = require("rss-libs/utils/http");
 
 const endpoint = createGenericEndpoint({
   endpointPath: "/cnn-international",
   entryUrl: "https://edition.cnn.com",
   skipPure: true,
-  translateTitle: true,
   maxItemsInList: 25,
   concurrency: 5,
-  fetchText: fetchTextWithCrossWallProxy,
   linkExtractor: linkExtractors.domAElementLinkExtractor(
     "a.container__title-url, a.container__link--type-article",
     "https://edition.cnn.com"
