0f8ab312844a8d90d815021c0b6d8c4f885c6b49
Theo <theo.sun@outlook.com>
2025-04-07 20:08:01 +0800

    chore: remove unused translateTitle, language, and fetchText options from BBC News endpoint

--- packages/rss-server/customize/bbc/index.js
+++ packages/rss-server/customize/bbc/index.js
@@ -4,10 +4,7 @@ const { fetchTextWithCrossWallProxy } = require("rss-libs/utils/http");
 const endpoint = createGenericEndpoint({
   feedTitle: "BBC News",
   endpointPath: "/bbc-news",
-  translateTitle: true,
-  language: "en",
   entryUrl: "https://feeds.bbci.co.uk/news/rss.xml",
-  fetchText: fetchTextWithCrossWallProxy,
   linkExtractor: linkExtractors.feedXmlLinkExtractor(),
   contentExtractor: contentExtractors.js,
 });
